02078bf2fb77ae96d3c8eeb2abb173d45fb223d6
michelelongo97 <m.longo7x@gmail.com>
2025-05-22 17:07:36 +0200

    feat: add snack 1

--- snacks.test.js
+++ snacks.test.js
@@ -1,1 +1,25 @@
 const {} = require("./snacks.js");
+
+function getInitials(nomecognome) {
+  const [nome, cognome] = nomecognome.split(" ");
+  return `${nome.charAt(0)}.${cognome.charAt(0)}.`;
+}
+//1
+test("La funzione getInitials restituisce le iniziali di un nome completo", () => {
+  expect(getInitials("Michele Longo")).toBe("M.L.");
+});
+
+//2
+// test('La funzione createSlug restituisce una stringa in lowercase')
+//3
+//  test('La funzione average calcola la media aritmetica di un array di numeri')
+//4
+//  test('La funzione createSlug sostituisce gli spazi con -.')
+///////////La funzione createSlug sostituisce gli spazi con -.
+//5
+//  test('La funzione isPalindrome verifica se una stringa è un palindromo.')
+//6
+//  test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido.')
+//7
+//  test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id"')
+///////////Creare uno o più test aggiuntivi che controllino che la struttura dati passati sia conforme (ogni post ha le proprietà id, title e slug, viene passato un id numerico).
